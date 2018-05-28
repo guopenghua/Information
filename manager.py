97a28f9c9c498a20e1b7953bf4c8e6424045af13
gph <gph@163.com>
2018-05-28 20:11:04 +0800

    分析模型类和迁移建表和准备数据 title左侧图标

--- manager.py
+++ manager.py
@@ -1,7 +1,6 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from info import create_app, db
-
+from info import create_app, db, models
 # 创建app
 app = create_app("dev")
 
@@ -15,4 +14,5 @@ manager.add_command("mysql", MigrateCommand)
 
 
 if __name__ == '__main__':
+    print(app.url_map)
     manager.run()
